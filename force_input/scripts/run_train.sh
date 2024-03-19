200adf996e770111f28c6ca9fb735492769d3e2a
yuboqiuab <163806716+yuboqiuab@users.noreply.github.com>
2024-03-18 22:15:30 -0500

    Add files via upload

--- force_input/scripts/run_train.sh
+++ force_input/scripts/run_train.sh
@@ -1,1 +1,15 @@
-
+python train.py \
+	--d_model 128 \
+	--h_dim 256 \
+	--num_layers 3 \
+	--conv_type 'cgcnn' \
+	-bs 8 \
+	-dr 1 \
+	-e 200 \
+	--optim 'adamw' \
+	-lr 1e-3 \
+	-wd 0 \
+	--gpu_idx 0 \
+	--deterministic True \
+	--log_wandb \
+	
