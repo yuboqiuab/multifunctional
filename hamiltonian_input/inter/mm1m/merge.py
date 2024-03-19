84eee8004d0f005ffe9d0f4c5484d242dc76321a
yuboqiuab <163806716+yuboqiuab@users.noreply.github.com>
2024-03-19 11:23:26 -0500

    Add files via upload

---
+++ hamiltonian_input/inter/mm1m/merge.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+with open('reduce_inputmm1m', 'r') as f1:
+    row1 = f1.readlines()
+with open('../mm2m/reduce_inputmm2m', 'r') as f2:
+    row2 = f2.readlines()
+
+
+f3 = open('reduce_mm', 'w');
+for i in range(len(row1)):
+   k1=row1[i].split()
+   for j1 in range(len(k1)):
+      f3.write("%9.6f \n" % (float(k1[j1])))
+   k2=row2[i].split()
+   for j2 in range(len(k2)):
+      f3.write("%9.6f \n" % (float(k2[j2])))
