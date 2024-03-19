d33d2c2f65169aabd96c3d1d5e4096504e8c7ec8
yuboqiuab <163806716+yuboqiuab@users.noreply.github.com>
2024-03-19 11:45:39 -0500

    Add files via upload

---
+++ hamiltonian_input/inter_ss_long/do.bash
@@ -0,0 +1,16 @@
+rm all_*
+python data.py
+
+for ((j=1;j<=662;j=j+1))
+    do
+    cat inputss1s_"$j"  >> all_inputss1s
+    cat inputss2s_"$j"  >> all_inputss2s
+    cat outputss_"$j"  >> all_outputss
+    cat vectorss_"$j"  >> all_vectorss
+    cat 25outputss_"$j"  >> all_25outputss
+    done
+
+
+cd ss1s
+bash do.bash
+cd ../
