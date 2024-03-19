a28e598554e2866f9da907aa3f9da28ff29b264e
yuboqiuab <163806716+yuboqiuab@users.noreply.github.com>
2024-03-18 22:48:45 -0500

    Add files via upload

---
+++ hamiltonian_input/intra_near/do.bash
@@ -0,0 +1,27 @@
+rm all_*
+python data.py
+
+for ((j=1;j<=662;j=j+1))
+    do
+    cat inputmm1m_"$j"  >> all_inputmm1m
+    cat inputmm2m_"$j"  >> all_inputmm2m
+    cat outputmm_"$j"  >> all_outputmm
+    cat vectormm_"$j"  >> all_vectormm
+
+    cat inputms1m_"$j"  >> all_inputms1m
+    cat inputms2s_"$j"  >> all_inputms2s
+    cat outputms_"$j"  >> all_outputms
+    cat vectorms_"$j"  >> all_vectorms
+
+    cat inputsm1s_"$j"  >> all_inputsm1s
+    cat inputsm2m_"$j"  >> all_inputsm2m
+    cat outputsm_"$j"  >> all_outputsm
+    cat vectorsm_"$j"  >> all_vectorsm
+
+    cat inputss1s_"$j"  >> all_inputss1s
+    cat inputss2s_"$j"  >> all_inputss2s
+    cat outputss_"$j"  >> all_outputss
+    cat vectorss_"$j"  >> all_vectorss
+
+    done
+
