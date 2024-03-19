9b723243a947a5390436ca9c6f9d5ccd327f60fa
yuboqiuab <163806716+yuboqiuab@users.noreply.github.com>
2024-03-18 22:33:09 -0500

    Add files via upload

---
+++ optimize_structure/step0/moforce/copy.bash
@@ -0,0 +1,12 @@
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_inter/short_mm1m
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_inter/short_mm2m
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_inter/short_ms1m/
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_inter/short_sm2m/
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_intra/short_sm2m/
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_intra/short_ms1m/
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_intra/short_mm1m/
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_intra/short_mm2m/
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_intra_long/short_mm2m/
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_intra_long/short_mm1m/
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_intra_long/short_ms1m/
+cp *.tar /mnt/data/yuboqi/code_all/h_learning/kinds_intra_long/short_sm2m/
