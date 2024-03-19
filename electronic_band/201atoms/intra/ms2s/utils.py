25aa2e69855cabc281ea8cb1915712e5aa6b9120
yuboqiuab <163806716+yuboqiuab@users.noreply.github.com>
2024-03-19 13:37:23 -0500

    Add files via upload

--- electronic_band/201atoms/intra/ms2s/utils.py
+++ electronic_band/201atoms/intra/ms2s/utils.py
@@ -1,1 +1,57 @@
+import torch
 
+
+class AverageMeter:
+    """Computes and stores the average and current value"""
+
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
+
+class ProgressMeter:
+
+    def __init__(self, num_batches, meters, prefix=""):
+        self.batch_fmtstr = self._get_batch_fmtstr(num_batches)
+        self.meters = meters
+        self.prefix = prefix
+
+    def display(self, batch):
+        entries = [self.prefix + self.batch_fmtstr.format(batch)]
+        entries += [str(meter) for meter in self.meters]
+        print('\t'.join(entries))
+
+    @staticmethod
+    def _get_batch_fmtstr(num_batches):
+        num_digits = len(str(num_batches // 1))
+        fmt = '{:' + str(num_digits) + 'd}'
+        return '[' + fmt + '/' + fmt.format(num_batches) + ']'
+
+
+class Normalizer:
+    def __init__(self, mean, std, device):
+        self.mean = torch.tensor(mean, device=device)
+        self.std = torch.tensor(std, device=device)
+
+    def normalize(self, tensor):
+        return tensor.sub_(self.mean).div_(self.std)
+
+    def denormalize(self, tensor):
+        return tensor.mul_(self.std).add_(self.mean)
