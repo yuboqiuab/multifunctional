1a9cf36f36d4a515a84743c886bff0102996abcd
yuboqiuab <163806716+yuboqiuab@users.noreply.github.com>
2024-03-19 10:56:17 -0500

    Add files via upload

---
+++ hamiltonian_input/intra_long/ss1s/pss1s.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+with open('../all_inputss1s', 'r') as f1:
+    row = f1.readlines()
+a=len(row)
+f2 = open('output', 'w');
+for i in range(int(a/51)):
+    f2.write("0.0000000   0.0000000   0.0000000\n")
